134ffcfb08dcd62b6ea0785b11ed2c37bcc72dc1
nghiaiuh <24643891.nghia@student.iuh.edu.vn>
2026-07-03 22:52:50 +0700

    delete: some files

--- apps/web/lib/constants.ts
+++
@@ -1,6 +0,0 @@
-export const OPENAI_IMAGES_URL = "https://api.openai.com/v1/images/generations";
-export const OPENAI_IMAGE_MODEL = "gpt-image-2";
-export const OPENAI_IMAGE_SIZE = "1024x1024";
-export const OPENAI_IMAGE_QUALITY = "high";
-export const OPENAI_IMAGE_OUTPUT_FORMAT = "png";
-export const OPENAI_IMAGE_PARTIAL_IMAGES = 2;
